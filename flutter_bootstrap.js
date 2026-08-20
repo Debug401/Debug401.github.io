f231a6380109da03ba71c8fec3e89230d8fab8e6
Debug401 <jamescap4456@gmail.com>
2026-08-20 12:33:52 +0800

    Change:Second Commit Deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "244436868" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1186193178" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
